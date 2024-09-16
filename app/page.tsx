5961dcfdc07baeb74a593a7a0f0388d13781b774
Jonathan Prince A <jonacoj29@gmail.com>
2024-09-16 12:07:04 +0530

    feat: added the passkey verification for the admin page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import PatientForm from "@/components/forms/PatientForm";
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
 
-const Home = () => {
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
 
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP VERIFICATION | PasskeyModal */}
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
@@ -23,7 +25,7 @@ const Home = () => {
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CareFlow
+              © 2024 CarePluse
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,32 +1,23 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import CustomFormField, { FormFieldType } from "../CustomFormField"
-import SubmitButton from "../SubmitButton"
-import { useState } from "react"
-import { UserFormValidation } from "@/lib/validation"
-import { useRouter } from "next/navigation"
-import { createUser } from "@/lib/actions/patient.actions"
+import { Form } from "@/components/ui/form";
+import { createUser } from "@/lib/actions/patient.actions";
+import { UserFormValidation } from "@/lib/validation";
 
+import "react-phone-number-input/style.css";
+import CustomFormField, { FormFieldType } from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
 
-export default function PatientForm() {
+export const PatientForm = () => {
   const router = useRouter();
-  const [isLoading,setIsLoading] = useState(false);
-  // 1. Define your form.
+  const [isLoading, setIsLoading] = useState(false);
+
   const form = useForm<z.infer<typeof UserFormValidation>>({
     resolver: zodResolver(UserFormValidation),
     defaultValues: {
@@ -34,8 +25,8 @@ export default function PatientForm() {
       email: "",
       phone: "",
     },
-  })
- 
+  });
+
   const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
     setIsLoading(true);
 
@@ -57,13 +48,15 @@ export default function PatientForm() {
 
     setIsLoading(false);
   };
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
         <section className="mb-12 space-y-4">
           <h1 className="header">Hi there 👋</h1>
           <p className="text-dark-700">Get started with appointments.</p>
         </section>
+
         <CustomFormField
           fieldType={FormFieldType.INPUT}
           control={form.control}
@@ -84,15 +77,16 @@ export default function PatientForm() {
           iconAlt="email"
         />
 
-         <CustomFormField
-              fieldType={FormFieldType.PHONE_INPUT}
-              control={form.control}
-              name="phone"
-              label="Phone number"
-              placeholder="(777) 1234-567"
-              />            
-          <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(555) 123-4567"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
-  )
-}
+  );
+};
